5107b2e63de7004ca31d32b7c926e75a40041ada
Mian Asif Riaz <asif@ikonami.net>
2019-12-07 18:26:26 +0500

    index.js -- Pakistan Zindabad

--- test/test.js
+++ test/test.js
@@ -1,8 +1,8 @@
 var request = require('supertest');
 var app = require('../index.js');
 describe('GET /', function() {
- it('respond with hey world', function(done) {
+ it('respond with Pakistan Zindabad', function(done) {
  //navigate to root and check the the response is "Hello world"
- request(app).get('/').expect('hey world', done);
+ request(app).get('/').expect('Pakistan Zindabad', done);
  });
 });
